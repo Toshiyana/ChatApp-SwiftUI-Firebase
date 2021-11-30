503a770c1ca1960cd77aaf55d8c47627b7df27f5
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-30 15:05:10 +0900

    Initial Commit

---
+++ ChatApp-SwiftUI-Firebase/ChatApp_SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp_SwiftUI_FirebaseApp.swift
+//  ChatApp-SwiftUI-Firebase
+//
+//  Created by Toshiyana on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp_SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
